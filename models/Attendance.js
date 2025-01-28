d8d78ebbb7e4db719f0a0b94eddbad94fa6a8ed3
Miguel <112570184+Miguellllllllll@users.noreply.github.com>
2025-01-28 07:13:08 -0500

    Attendance.js

--- models/Attendance.js
+++ models/Attendance.js
@@ -1,16 +1,11 @@
 const mongoose = require('mongoose');
 
 const formatDateTime = (date) => {
-    const dd = String(date.getDate()).padStart(2, '0');
-    const mm = String(date.getMonth() + 1).padStart(2, '0'); // Enero es 0!
-    const yy = String(date.getFullYear()).slice(-2);
-    
-    let hh = date.getHours();
-    const min = String(date.getMinutes()).padStart(2, '0');
-    const ampm = hh >= 12 ? 'PM' : 'AM'; // Determinar AM o PM
-    hh = hh % 12 || 12; // Convertir 0-23h a 12h formato (0 → 12, 13 → 1)
+    const formatDateTime = (date) => {
+  const options = { timeZone: 'America/Lima', hour12: true, year: '2-digit', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+  return new Intl.DateTimeFormat('es-PE', options).format(date).replace(',', ' -');
+};
 
-    return `${dd}/${mm}/${yy} - ${hh}:${min} ${ampm}`;
 };
 const generateId = () => {
   return Math.floor(1000 + Math.random() * 9000); // Genera un número de 4 dígitos
