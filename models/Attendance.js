b0b1cc3c79270ddd400ff44e727dacc49ded1147
Miguel <112570184+Miguellllllllll@users.noreply.github.com>
2025-01-28 07:24:35 -0500

    Attendance.js

--- models/Attendance.js
+++ models/Attendance.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const formatDateTime = (date) => {
-    console.log(date)
     const dd = String(date.getDate()).padStart(2, '0');
     const mm = String(date.getMonth() + 1).padStart(2, '0'); // Enero es 0!
     const yy = String(date.getFullYear()).slice(-2);
@@ -10,18 +9,17 @@ const formatDateTime = (date) => {
     return `${dd}/${mm}/${yy}-${hh}:${min}`;
 };
 
-};
 const generateId = () => {
   return Math.floor(1000 + Math.random() * 9000); // Genera un número de 4 dígitos
 };
 
 const attendanceSchema = new mongoose.Schema({
-  _id: { type: Number, default: generateId },
-  name: { type: String, required: true },
-  dateTime: { 
+    _id: { type: Number, default: generateId },
+    name: { type: String, required: true },
+    dateTime: { 
     type: String, 
     default: () => formatDateTime(new Date()) 
-  }
+    }
 });
 
 attendanceSchema.index({ name: 1, dateTime: 1 }, { unique: true });
