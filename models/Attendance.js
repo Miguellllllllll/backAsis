be5ce11ec45fc68ff9d0743d25107ee870e3208a
Miguel <112570184+Miguellllllllll@users.noreply.github.com>
2025-01-28 06:52:15 -0500

    Attendance.js

--- models/Attendance.js
+++ models/Attendance.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const formatDateTime = (date) => {
-    const dd = String(date.getDate()).padStart(2, '0');
-    const mm = String(date.getMonth() + 1).padStart(2, '0'); // Enero es 0!
-    const yy = String(date.getFullYear()).slice(-2);
-    const hh = String(date.getHours()).padStart(2, '0');
-    const min = String(date.getMinutes()).padStart(2, '0');
-    return `${dd}/${mm}/${yy} ${hh}:${min}`;
+  const dd = String(date.getDate()).padStart(2, '0');
+  const mm = String(date.getMonth() + 1).padStart(2, '0'); // Enero es 0!
+  const yy = String(date.getFullYear()).slice(-2);
+  const hh = String(date.getHours()).padStart(2, '0');
+  const min = String(date.getMinutes()).padStart(2, '0');
+  return `${dd}/${mm}/${yy}-${hh}:${min}`;
 };
 
 const generateId = () => {
@@ -14,12 +14,12 @@ const generateId = () => {
 };
 
 const attendanceSchema = new mongoose.Schema({
-    _id: { type: Number, default: generateId },
-    name: { type: String, required: true },
-    dateTime: { 
+  _id: { type: Number, default: generateId },
+  name: { type: String, required: true },
+  dateTime: { 
     type: String, 
     default: () => formatDateTime(new Date()) 
-    }
+  }
 });
 
 attendanceSchema.index({ name: 1, dateTime: 1 }, { unique: true });
